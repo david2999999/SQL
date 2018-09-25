2e4807386d15cb8b4826b2b6f599a45eeee6ba48
David Jiang <djiang86@binghamton.edu>
2018-09-25 22:24:32 +0000

    insert

---
+++ Basic/Insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO customers (id, name, age, address, salary)
+    VALUES (1, 'Kim', 32, 'Louis', 2000.00);
+    
+INSERT INTO customers (id, name, age, address, salary)
+    VALUES (1, 'Qwety', 32, 'Louiasdqwds', 2000.00);
+
+INSERT INTO customers (id, name, age, address, salary)
+    VALUES (1, 'ASdasd', 32, 'Louasdis', 2000.00);
