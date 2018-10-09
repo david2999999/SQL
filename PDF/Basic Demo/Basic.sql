5dd89444b3593270a1cf6f4516aec6bfdddc189c
David Jiang <djiang86@binghamton.edu>
2018-10-09 21:14:48 +0000

    simple join

---
+++ PDF/Basic Demo/Basic.sql
@@ -0,0 +1,12 @@
+CREATE TABLE corporation (
+    corp_id SMALLINT,
+    name VARCHAR(30),
+    CONSTRAINT pk_corporation PRIMARY KEY(corp_id)
+);
+
+INSERT INTO corporation(corp_id, name) 
+    VALUES (27, 'Acme Paper Corporation');
+
+SELECT name 
+    FROM corporation
+    WHERE corp_id = 27;
