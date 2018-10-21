a3361f0ebd902d2f43fe0c7889ceabb75d6888e8
David Jiang <djiang86@binghamton.edu>
2018-10-21 04:19:08 +0000

    creatae table DDL

---
+++ Dummy PDF/DDL/Create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE CUSTOMER (
+    CustomerID INTEGER NOT NULL,
+    FirstName CHAR (15),
+    LastName CHAR (20) NOT NULL,
+    Street CHAR (25),
+    City CHAR (20),
+    State CHAR (2),
+    Zipcode CHAR (10),
+    Phone CHAR (13) 
+) ;
