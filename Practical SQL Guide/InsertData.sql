8d7a75f246f2f369267454cee22e1c54357d17d9
David Jiang <djiang86@binghamton.edu>
2018-11-21 14:00:03 +0000

    inserting data into db

---
+++ Practical SQL Guide/InsertData.sql
@@ -0,0 +1,20 @@
+Insert Into Products (cod_product, description, unit_price)
+    Values (1, 'Eggs', 2.49);
+Insert Into Products (cod_product, description, unit_price)
+    Values (2, 'Ice Cream', 3.99);
+Insert Into Products (cod_product, description, unit_price)
+    Values (3, 'Soda', 0.65);
+Insert Into Products (cod_product, description, unit_price)
+    Values (4, 'Cheese', 2.89);
+Insert Into Products (cod_product, description, unit_price)
+    Values (5, 'Pork Meat', 3.10);
+Insert Into Customers (cod_customer, name)
+    Values (1, 'Anne');
+Insert Into Customers (cod_customer, name)
+    Values (2, 'Peter');
+Insert Into Customers (cod_customer, name)
+    Values (3, 'Elena');
+Insert Into Customers (cod_customer, name)
+    Values (4, 'Shirley');
+Insert Into Customers (cod_customer, name)
+    Values (5, 'John');
