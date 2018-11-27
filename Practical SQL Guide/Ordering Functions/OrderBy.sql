ea462b5a0ec3457f6f72422ed297695d09a26013
David Jiang <djiang86@binghamton.edu>
2018-11-27 21:20:38 +0000

    orderby

---
+++ Practical SQL Guide/Ordering Functions/OrderBy.sql
@@ -0,0 +1,12 @@
+-- This instruction selects all columns of Customers table ordering the records by name
+-- (ascending order).
+Select *
+From Customers
+Order By Name;
+
+
+-- This instruction selects all columns of Customers table ordering the records by name
+-- (descending order).
+Select *
+From Customers
+Order By Name DESC;
